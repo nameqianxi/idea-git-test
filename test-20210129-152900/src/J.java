d1f43d063e1c1d78150b885c2a65c1dcf543970e
qianxi <qianx@hopshine.net>
2021-01-29 15:30:24 +0800

    【新增】又新建了模块

---
+++ test-20210129-152900/src/J.java
@@ -0,0 +1,11 @@
+/**
+ * @author qx
+ * @desc
+ * @date 2021/1/29
+ * @copyright ©2021 All Right Reserved
+ */
+public class J {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
